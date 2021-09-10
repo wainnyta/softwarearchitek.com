b7a16f26bf59940c5adecc4e0e505119e58cafdf
Meer Bahadin <meerbahadin10@gmail.com>
2021-09-10 19:45:41 +0300

    creating single blog page

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,7 +1,9 @@
 import BlogLayout from "components/BlogLayout";
+import { useRouter } from "next/router";
 
 const Blog = () => {
-  return <BlogLayout>hello</BlogLayout>;
+  const router = useRouter();
+  return <BlogLayout>{JSON.stringify(router.query)}</BlogLayout>;
 };
 
 export default Blog;
