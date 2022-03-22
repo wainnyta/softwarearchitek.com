6ba88df39bcc21c6b51d1bcfbb423c1de02ccfbf
Wainny Ta <wainny@globalofficesoftware.com>
2022-03-21 23:21:02 -0500

    Added Google Analytics Tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,10 +4,18 @@ import theme from "theme";
 import Layout from "components/Layout";
 import "@fontsource/nunito/";
 import 'react-image-lightbox/style.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ModalContact from "../components/pages/ModalContact";
 import {DefaultSeo} from "next-seo";
 import AppSEO from "../components/AppSEO";
+import Script from 'next/script';
+import TagManager from "react-gtm-module/dist/TagManager";
+
+
+const tagManagerArgs = {
+  gtmId: process.env.NEXT_PUBLIC_GTM_TAG,
+};
+
 
 function MyApp({ Component, pageProps }) {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -19,27 +27,49 @@ function MyApp({ Component, pageProps }) {
     message: '',
   });
 
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <DefaultSeo {...AppSEO} />
-        <Component
-          {...pageProps}
-          isOpen={isOpen}
-          onOpen={onOpen}
-          onClose={onClose}
-          initialValues={initialValues}
-          setInitialValues={setInitialValues}
-        />
-        <ModalContact
-          isOpen={isOpen}
-          onOpen={onOpen}
-          onClose={onClose}
-          setInitialValues={setInitialValues}
-          initialValues={initialValues}
-        />
-      </Layout>
-    </ChakraProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
+
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <DefaultSeo {...AppSEO} />
+          <Component
+            {...pageProps}
+            isOpen={isOpen}
+            onOpen={onOpen}
+            onClose={onClose}
+            initialValues={initialValues}
+            setInitialValues={setInitialValues}
+          />
+          <ModalContact
+            isOpen={isOpen}
+            onOpen={onOpen}
+            onClose={onClose}
+            setInitialValues={setInitialValues}
+            initialValues={initialValues}
+          />
+        </Layout>
+      </ChakraProvider>
+    </>
+
   );
 }
 
